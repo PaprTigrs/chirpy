34ad4d7d992f929faeb3dabe242bdd8ed0bd2b83
PaprTigrs <rb411@hotmail.com>
2026-03-09 13:55:17 -0500

    adding handlerMetrics

--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	server := http.Server{
+	mux.HandleFunc("/healthz", readinessHandler)
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/",
+		http.FileServer(http.Dir(".")))),
+	)
+
+	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
